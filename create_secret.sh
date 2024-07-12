2d6e5cd9e426c5d90ae8db4b780ea3ba6389675d
Andre Bush <andrescottbush@hotmail.com>
2024-07-11 21:19:58 -0700

    updated build scripts for docker

--- create_secret.sh
+++ create_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Path to your secrets .env file
+# Path to secrets .env file
 SECRETS_FILE="envs.env_file"
 
 # Check if Docker Swarm is initialized
@@ -10,15 +10,41 @@ if [ $? -ne 0 ]; then
     docker swarm init
 fi
 
-# Create Docker secrets from the .env file
-while IFS='=' read -r key value; do
+# Function to create a Docker secret
+create_secret() {
+    local key=$1
+    local value=$2
+
+    echo "Creating secret for $key..."
+    echo -n $value | docker secret create $key -
+    if [ $? -ne 0 ]; then
+        echo "Error creating secret for $key."
+    fi
+}
+
+# Stop all services
+services=$(docker service ls --format '{{.Name}}')
+for service in $services; do
+    echo "Stopping service $service..."
+    docker service rm $service
+done
+
+# Remove existing secrets
+while IFS='=' read -r key _; do
     if [ ! -z "$key" ]; then
-        echo "Creating secret for $key..."
-        echo -n $value | docker secret create $key -
-        if [ $? -ne 0 ]; then
-            echo "Error creating secret for $key. It might already exist."
-        fi
+        echo "Removing secret $key if it exists..."
+        docker secret rm $key 2>/dev/null
     fi
 done < "$SECRETS_FILE"
 
-echo "Secrets creation completed."
+# Create new secrets from the .env file
+while IFS='=' read -r key value; do
+    if [ ! -z "$key" ]; then
+        create_secret $key $value
+    fi
+done < "$SECRETS_FILE"
+
+# Start all services
+./docker_build.sh
+
+echo "Secrets creation or update completed and services restarted."
