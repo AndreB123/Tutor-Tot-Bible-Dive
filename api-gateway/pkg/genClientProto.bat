ddf71768b36e0b34e9cadfa745dc620634fcb8b4
Andre Bush <andrescottbush@hotmail.com>
2024-07-16 19:26:15 -0700

    updated proto get scripts and protos for api gateway

--- api-gateway/pkg/genClientProto.bat
+++ api-gateway/pkg/genClientProto.bat
@@ -1,2 +1,3 @@
 protoc --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative proto/chat_service.proto
 protoc --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative proto/user_service.proto
+protoc --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative proto/lesson_service.proto
